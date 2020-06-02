fe8418c84b56fe803c8d39672f8c5a6a7bd9c6c9
gomezroo <omar-andres.gomez-rosales@grenoble-inp.org>
2020-06-02 10:33:33 +0200

    pokedex with cards that show pokemon image and name from static list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainComponent } from "./pages/main/main.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: MainComponent },
+  { path: "**", pathMatch: "full", redirectTo: "home" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"]
+})
+export class MainComponent implements OnInit {
+  pokemons = [
+    {
+      name: "Pikachu",
+      url: "https://image.flaticon.com/icons/svg/188/188987.svg"
+    },
+    {
+      name: "Bulbasur",
+      url: "https://image.flaticon.com/icons/svg/188/188989.svg"
+    },
+    {
+      name: "Charmander",
+      url: "https://image.flaticon.com/icons/svg/188/188990.svg"
+    },
+    {
+      name: "Snorlax",
+      url: "https://image.flaticon.com/icons/svg/189/189001.svg"
+    },
+    {
+      name: "Eevee",
+      url: "https://image.flaticon.com/icons/svg/188/188995.svg"
+    },
+    {
+      name: "Squirtle",
+      url: "https://image.flaticon.com/icons/svg/188/188988.svg"
+    },
+    {
+      name: "Meowth",
+      url: "https://image.flaticon.com/icons/svg/188/188997.svg"
+    },
+    {
+      name: "Psyduck",
+      url: "https://image.flaticon.com/icons/svg/189/189000.svg"
+    },
+    {
+      name: "Jigglypuff",
+      url: "https://image.flaticon.com/icons/svg/188/188993.svg"
+    }
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
